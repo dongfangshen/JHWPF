86f909ad30f490238c029fd726426b67661cece7
JamesHu <513826883@qq.com>
2017-07-17 20:18:21 +0800

    chapter02

---
+++ Chapter/Chapter02/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        private async void cmdAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            TuLinHelper helper = new TuLinHelper();
+            var result = await helper.GetText(txtQuestion.Text.ToString());
+            if (!string.IsNullOrWhiteSpace(txtAnswer.Text))
+            {
+                sb.AppendLine();
+            }
+            sb.Append(DateTime.Now.ToString());
+            sb.AppendLine();
+            sb.Append(result);
+            txtAnswer.Text = sb.ToString();
+        }
+    }
+
+}
---
+++ Chapter/Chapter02/WpfApplication1/WpfApplication1/TuLinHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using DalSoft.RestClient;
+
+namespace WpfApplication1
+{
+    public class TuLinHelper
+    {
+        //api地址 http://www.tuling123.com/openapi/api
+        //api key 1ad7635d3dbf45af8661abdc2dbf4264
+        //secret 54a51d0dbdc467ee
+        private readonly string apiUrl = "http://www.tuling123.com/openapi/api";
+        private readonly string apiKey = "1ad7635d3dbf45af8661abdc2dbf4264";
+        public async Task<string> GetText(string info)
+        {
+            HttpClient client = new HttpClient();
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("key", apiKey);
+            dic.Add("info", info);
+            var response = await client.PostAsync(apiUrl, new FormUrlEncodedContent(dic));
+            var responseValue = await response.Content.ReadAsStringAsync();
+            JObject obj = JObject.Parse(responseValue);
+            if (obj.Count > 0)
+            {
+                return obj["text"]?.ToString() ?? "无内容";
+            }
+            return "调用无消息";
+        }
+
+      
+    }
+}
